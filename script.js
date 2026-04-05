748e0cd3ff31de8e87d1b1bca0a3b0e8a34bfbb3
sabeeha shaik <142984283+sabeeha1234@users.noreply.github.com>
2026-04-05 18:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const size = document.getElementById('size')
+   function DisplayWindowSize(){
+        const width = window.innerWidth //innerwidth is a property for findig inner width , if its like innerwidth() this then its called method
+         const height = window.innerHeight
+
+         size.innerText = `width is ${width} and height is ${height}`
+
+          
+
+// call once when page loads
+DisplayWindowSize()
+    }
+      window.addEventListener('resize', DisplayWindowSize) 
